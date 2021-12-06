4251d578f04759e6b4c422e46c29d63518166432
Ye Shu <hello@shuye.dev>
2021-12-05 22:01:31 -0500

    Add comments to Infrastructor & Config class
    The method docstrings conform to PEP 257 standard.
    The attribute docstring conform to PEP 224 & PEP 258 standards (which are both rejected but seem to be the de facto standard)

--- Infrastructor.py
+++ Infrastructor.py
@@ -17,6 +17,15 @@ _VT = TypeVar("_VT")
 # this makes a copy
 def merge_dicts(base_dict: Dict[_KT, _VT], update_with: Dict[_KT, _VT]) -> Dict[
     _KT, _VT]:
+    """Merges two dictionaries into a new dictionary.
+
+    In the case of a conflict, the value in base_dict will be overwritten by
+    value in update_with.
+
+    :param base_dict: The base dictionary
+    :param update_with: The dictionary to be updated with
+    :return: A new dictionary that merges the content of two dictionary.
+    """
     d = {}
     d.update(base_dict)
     d.update(update_with)
@@ -24,13 +33,32 @@ def merge_dicts(base_dict: Dict[_KT, _VT], update_with: Dict[_KT, _VT]) -> Dict[
 
 
 class Infrastructor(object):
+    """The main class for Infrastructor
+
+    Contains methods that takes Config class and executes logic
+    """
+
     @staticmethod
     def usage(pname: str) -> None:
+        """Prints out the usage for the executable
+
+        :type pname: Name of the current executable
+        """
         print(f"Usage: {pname} [flags] <json config file>", file=sys.stderr)
         print("\twhere flags are:", file=sys.stderr)
         print("\t-v\tverbose mode; print debug output.", file=sys.stderr)
 
     def argparse(self, args: Sequence[str]) -> Dict[str, Any]:
+        """Calls rec_argparse to parse the input arguments
+
+        Returns a dictionary of
+        ```
+        { "verbose": False, "json_conf_file": "<filename>.conf" }
+        ```
+
+        :param args: list of input arguments
+        :return: a dictionary of keywords and their values
+        """
         pname = os.path.basename(args[0])
         # convert to list and strip program name
         # xs = list(args)
@@ -65,6 +93,11 @@ class Infrastructor(object):
                 sys.exit(1)
 
     def __init__(self, args: Sequence[str]):
+        """
+        Initializes the Infrastructor given a list of input arguments
+
+        :param args: list of input arguments
+        """
         print(args)
         # self check
         self_check()
@@ -72,13 +105,22 @@ class Infrastructor(object):
         # get arguments
         opts = self.argparse(args)
         self.verbose: bool = opts["verbose"]
+        "Whether to enable verbose output"
 
+        # TODO: extend the class to read in multiple config files at once?
         self.config = Config(opts["json_conf_file"])
+        "The Config object representing the config file in input argument"
 
     @staticmethod
     def pull_all(config: Config, basepath: str, use_user_name: bool,
                  anonymize: bool) -> None:
-        # pull all repositories into archive and submission dirs
+        """Pulls all repositories into archive and submission dirs
+
+        :param config: The Config object for the assignment
+        :param basepath: Pate to base directory
+        :param use_user_name: Whether to use username as part of path
+        :param anonymize: Whether to anonymize reponame
+        """
         for repo in config.repositories:
             rpath = config.pull_path(basepath, repo, use_user_name, anonymize)
             if not os.path.exists(rpath):
@@ -87,20 +129,23 @@ class Infrastructor(object):
                 call(["git", "clone", config.repo_ssh_path(repo), rpath])
             else:  # existing repository
                 # make sure we're on the default branch
-                print(f"Switching to '{config.default_branch}' branch in {config.repo_ssh_path(repo)} at {rpath}")
+                print(f"Switching to '{config.default_branch}' branch in "
+                      f"{config.repo_ssh_path(repo)} at {rpath}")
                 Popen(["git", "checkout", config.default_branch],
                       cwd=rpath).wait()  # note: blocking
 
                 # first reset repository
                 print(f"Resetting {config.repo_ssh_path(repo)} at {rpath}")
-                Popen(["git", "checkout", "."], cwd=rpath).wait()  # note: blocking
+                Popen(["git", "checkout", "."],
+                      cwd=rpath).wait()  # note: blocking
 
                 # pull it
                 print(f"Pulling {config.repo_ssh_path(repo)} in {rpath}")
                 Popen(["git", "pull"], cwd=rpath).wait()  # note: blocking
 
             # if a due date was specified, roll back to due date
-            if hasattr(config, "do_not_accept_changes_after_due_date_timestamp"):
+            if hasattr(config,
+                       "do_not_accept_changes_after_due_date_timestamp"):
                 proc = Popen(
                     ["git",
                      "rev-list",
@@ -120,6 +165,15 @@ class Infrastructor(object):
 
     @staticmethod
     def push_starter(config: Config) -> None:
+        """Pushes the starter repo to all student repositories.
+
+        Starter code is distributed by setting each student repository as a
+        "remote" for the starter repository and then `push`ing.  Student
+        repositories _must_ be empty (i.e., no `main` branch) otherwise
+        `push` will fail.
+
+        :param config: The Config object for the assignment
+        """
         print(f"starter repo is: {config.starter_repo}")
         for repo in config.repositories:
             actual_repo = config.repo_ssh_path(repo)
@@ -130,6 +184,13 @@ class Infrastructor(object):
 
     def copy_to_ta_folders(self, config: Config, ta_home: str, ta_dirname: str,
                            basepath: str) -> None:
+        """ Copies all local repositories to TA directories for grading
+
+        :param config: The Config object for the assignment
+        :param ta_home: Path to home directory for all TA grading
+        :param ta_dirname: Name of directory for this assignment
+        :param basepath: Base path where local repositories are stored
+        """
         # keep track of repository -> TA map and print out the key
         # after doing all of the copying
         ta_map = []
@@ -163,6 +224,13 @@ class Infrastructor(object):
 
     def copy_from_ta_folders(self, config: Config, ta_home: str,
                              ta_dirname: str, basepath: str) -> None:
+        """ Copies all local repositories back from TA directories after grading
+
+        :param config: The Config object for the assignment
+        :param ta_home: Path to home directory for all TA grading
+        :param ta_dirname: Name of directory for this assignment
+        :param basepath: Base path for local repositories to be copied to
+        """
         # cp all files except git stuff
         for repo in config.repositories:
             # compute target
@@ -194,6 +262,12 @@ class Infrastructor(object):
 
     @staticmethod
     def branch_exists(config: Config, rdir: str) -> bool:
+        """Checks if the feedback branch exists in a local repository
+
+        :param config: The Config object for the assignment
+        :param rdir: Path to a local repository
+        :return: If the feedback branch exists.
+        """
         # check to see if FEEDBACK_BRANCH branch exists
         proc = Popen(["git", "show-ref", "--verify", "--quiet",
                       "refs/heads/" + config.feedback_branch],
@@ -204,6 +278,11 @@ class Infrastructor(object):
         return proc.returncode == 0
 
     def commit_changes(self, config: Config, basepath: str) -> None:
+        """Commits changes to the feedback branch in all repos
+
+        :param config: The Config object for the assignment
+        :param basepath: basepath for local path of repositories
+        """
         for repo in config.repositories:
             # get submissions dir path for repo
             rdir = config.pull_path(basepath, repo, False,
@@ -229,6 +308,14 @@ class Infrastructor(object):
 
     def issue_pull_request(self, config: Config, reponame: str,
                            org: Organization) -> int:
+        """Push local feedback branch to remote and issue pull request to the
+        default branch for the specified repo
+
+        :param config: The Config object for the assignment
+        :param reponame: Name of the repository
+        :param org: A github.Organization object
+        :return: an error number defined in errno; or 0 if no error.
+        """
         # extract basename
         bn = os.path.basename(reponame)
 
@@ -262,7 +349,8 @@ class Infrastructor(object):
         #### Lida: needed to edit since reponame contains the relative path, not just the name ####
         # Dan/Bill: not sure why this is necessary... hopefully we trigger it again and figure out why...
         # Popen(["git", "push", "origin", self.feedback_branch], cwd=reponame).wait()
-        Popen(["git", "push", "origin", config.feedback_branch], cwd=rdir).wait()
+        Popen(["git", "push", "origin", config.feedback_branch],
+              cwd=rdir).wait()
 
         # create pull request
         if self.verbose:
--- config.py
+++ config.py
@@ -10,29 +10,57 @@ from utils import canonical_group_name, java_string_hashcode, round_robin_map
 
 
 class Config(object):
-    # config JSON
-    # {
-    #  "default_branch" : "master",
-    #  "feedback_branch" : "TA-feedback",
-    #  "hostname" : "github-wcs",
-    #  "course" : "cs334",
-    #  "assignment_name" : "hw0",
-    #  "do_not_accept_changes_after_due_date_timestamp" : 1518671700,
-    #                                                     # a UNIX timestamp
-    #  "anonymize_sub_path" : true,
-    #  "archive_path" : "/home/example/archive",
-    #  "submission_path" : "/home/example/submission",
-    #  "ta_path" : "/home/example/tas",
-    #  "starter_repo" : "/home/example/starter-repo.git",
-    #  "github_org" : "williams-cs",
-    #  "TAs" : [ "ta_1", "ta_2", ..., "ta_n" ],
-    #  "repository_map" : {
-    #    "student_1" : "repo_1",
-    #    "student_2" : "repo_2",
-    #    ...
-    #    "student_n" : "repo_n"
-    #  }
-    # }
+    """
+    A data-only class storing the parsed Infrastructor configuration for an
+    assignment
+
+    This class reads in an Infrastructor JSON configuration file with a
+    specified path and parses them into a list of
+
+    config JSON should be in the following format
+    ```json
+    {
+     "default_branch" : "master",
+     "feedback_branch" : "TA-feedback",
+     "hostname" : "github-wcs",
+     "course" : "cs334",
+     "assignment_name" : "hw0",
+     "do_not_accept_changes_after_due_date_timestamp" : 1518671700,
+                                                        # a UNIX timestamp
+     "anonymize_sub_path" : true,
+     "archive_path" : "/home/example/archive",
+     "submission_path" : "/home/example/submission",
+     "ta_path" : "/home/example/tas",
+     "starter_repo" : "/home/example/starter-repo.git",
+     "github_org" : "williams-cs",
+     "TAs" : [ "ta_1", "ta_2", ..., "ta_n" ],
+     "repository_map" : {
+       "student_1" : "repo_1",
+       "student_2" : "repo_2",
+       ...
+       "student_n" : "repo_n"
+     }
+    }
+    ```
+
+    Attributes:
+        hostname (str): The name of your SSH `config` host to use for script interaction. This allows you to use a different GitHub identity for managing course scripts because, presently, PyGithub does not support two-factor authentication.
+        user2repo (Dict[str, str]): A dictionary mapping user to their repository name.
+        repo2group (Dict[str, List[str]]): A dictionary mapping repo name to the list of students (assuming group assignment). The list will be length 1 if individual assignment.
+        ta_assignments (Dict[str, str]): A dictionary mapping each repository to the grading TAs.
+        course (str): The name of the course.
+        assignment_name (str): The name of the assignment
+        starter_repo (str): Path to starter repo.  Starter code is distributed by setting each student repository as a "remote" for the starter repository and then `push`ing.  Student repositories _must_ be empty (i.e., no `main` branch) otherwise `push` will fail.
+        github_org (str): Name of the GitHub organization to use.
+        archive_path (str): Path to folder intended as deanonymized repository of student submissions for Academic Honor Code cases.
+        submission_path (str): Path to faculty-only staging area for squashing and modifying TA feedback before issuing pull requests.
+        ta_path (str): Path to TA staging area where anonymized student submissions are copied.
+        feedback_branch (str): Branch to commit TA/instructor feedback on. Pull requests are issued from this branch.
+        default_branch (str): Branch that student commits to. Defaults to `main` if not specified.
+        due_date (int): Optional. a UNIX timestamp representing the due date in the local timezone.
+        anonymize_sub_path (bool): whether the contents of the `submissions` folder, which is viewable only by faculty (not TAs), is anonymized.
+        rsync_excludes (List[str]): List of files & directories to be excluded from rsync when copying to TA folder.
+    """
 
     def __init__(self, json_conf_file: str):
 
@@ -42,27 +70,77 @@ class Config(object):
             conf = json.loads(f.read())
 
         # declare/init fields
+
         self.hostname: str = conf["hostname"]
+        """
+        The name of your SSH `config` host to use for script
+        interaction. This allows you to use a different GitHub identity for
+        managing course scripts because, presently, PyGithub does not support
+        two-factor authentication.
+        """
+
         self.user2repo: Dict[str, str] = {}
+        "A dictionary mapping user to their repository name."
+
         self.repo2group: Dict[str, List[str]] = {}
+        """A dictionary mapping repo name to the list of students (assuming
+        group assignment). The list will be length 1 if individual assignment.
+        """
+
         self.ta_assignments: Dict[str, str] = {}
+        "A dictionary mapping each repository to the grading TAs."
+
         self.course: str = conf["course"]
+        "The name of the course."
+
         self.assignment_name: str = conf["assignment_name"]
+        "The name of the assignment."
+
         self.starter_repo: str = conf["starter_repo"]
+        """
+        Path to starter repo.  Starter code is distributed by setting each
+        student repository as a "remote" for the starter repository and then
+        `push`ing.  Student repositories _must_ be empty (i.e., no `main`
+        branch) otherwise `push` will fail.
+        """
+
         self.github_org: str = conf["github_org"]
+        "Name of the GitHub organization to use."
+
         self.archive_path: str = conf["archive_path"]
+        """Path to folder intended as deanonymized repository of student
+        submissions for Academic Honor Code cases."""
+
         self.submission_path: str = conf["submission_path"]
+        """Path to faculty-only staging area for squashing and modifying TA
+        feedback before issuing pull requests."""
+
         self.ta_path: str = conf["ta_path"]
+        """Path to TA staging area where anonymized student submissions are
+        copied."""
+
         self.feedback_branch: str = conf["feedback_branch"]
+        """Branch to commit TA/instructor feedback on. Pull requests are
+        issued from this branch."""
+
         self.default_branch: str = conf["default_branch"] \
             if "default_branch" in conf else "main"
+        "Branch that student commits to. Defaults to `main` if not specified."
+
         if "do_not_accept_changes_after_due_date_timestamp" in conf:
-            # TODO: type? best guess is str
+            # TODO: type? best guess is int
             self.due_date = \
                 conf["do_not_accept_changes_after_due_date_timestamp"]
+            "A UNIX timestamp representing the due date in the local timezone."
+
         self.anonymize_sub_path: bool = conf["anonymize_sub_path"] \
             if "anonymize_sub_path" in conf else True
+        """ whether the contents of the `submissions` folder, which is viewable
+        only by faculty (not TAs), is anonymized."""
+
         self.rsync_excludes: List[str] = conf["rsync_excludes"]
+        """List of files & directories to be excluded from rsync when copying
+        to TA folder."""
 
         # populate mappings (user2repo, repo2group)
         for student in conf["repository_map"].keys():
@@ -80,9 +158,21 @@ class Config(object):
 
     @property
     def list_of_users(self) -> List[str]:
+        """Returns a list of all users in the course
+
+        :rtype: List[str]
+        :return: a list of all users in the course
+        """
         return list(self.user2repo.keys())
 
     def add_mapping(self, user: str, repo: str) -> None:
+        """An internal method used to initialize self.user2repo and
+        self.repo2group attributes
+
+        :param user: Name of student
+        :param repo: Name of repository
+        """
+
         # pair user with repo
         self.user2repo[user] = repo
 
@@ -97,12 +187,25 @@ class Config(object):
             self.repo2group[repo] = [user]
 
     def lookupGroup(self, repo: str) -> List[str]:
+        """Looks up the student(s) assigned to the repo
+
+        :param repo: Name of the repository
+        :rtype: List[str]
+        :return: A list of users assigned to the repo
+        """
         return self.repo2group[repo]
 
     def lookupRepo(self, user: str) -> str:
+        """Looks up the repository the user is assigned to
+
+        :param user: Name of the user
+        :rtype: str
+        :return: Name of the repository
+        """
         return self.user2repo[user]
 
     def pretty_print(self) -> None:
+        """Prints out a human-readable representation of the configuration"""
         print("Student -> repository map:")
         for user in self.user2repo.keys():
             print(f"  {user} -> {self.user2repo[user]}")
@@ -125,19 +228,43 @@ class Config(object):
 
     @property
     def repositories(self) -> List[str]:
+        """Returns a list of sorted repositories of this assignment
+
+        :rtype: List[str]
+        :return: A list of sorted repositories of this assignment
+        """
         # sorting ensures that repository order is deterministic
         repos = list(self.repo2group.keys())
         return sorted(repos)
 
     def repo_ssh_path(self, repo: str) -> str:
-        # return "git@" + self.hostname + ":" + self.github_org + "/" + repo + ".git"
+        """Returns the Git SSH path of a given repo
+
+        :param repo: Name of the repo
+        :rtype: str
+        :return: Git SSH path of a given repository
+        """
         return f"git@{self.hostname}:{self.github_org}/{repo}.git"
 
     def lookupTA(self, repo: str) -> str:
+        """Looks up the TA assigned to grade the repo
+
+        :param repo: Name of the repo
+        :rtype: Name of the TA
+        :return: Name of the TA assigned to grade the repo
+        """
         return self.ta_assignments[repo]
 
     def pull_path(self, basepath: str, repo: str, use_user_name: bool,
                   anonymize: bool) -> str:
+        """Finds out the local path for the repository
+
+        :param basepath: basepath for local path (depending on category)
+        :param repo: Name of the repository
+        :param use_user_name: Whether user name is part of the path
+        :param anonymize: Whether reponame is anonymized
+        :return: The local path of the specified repository
+        """
         # if anonymize, then get the SHA1 hash of the repo name
 
         reponame = hashlib.sha1(repo.encode(
@@ -150,14 +277,28 @@ class Config(object):
         else:
             return os.path.join(basepath, reponame)
 
-    # this method anonymizes the repository name
     def TA_target(self, ta_home: str, ta_dirname: str, repo: str) -> str:
+        """Finds out the anonymized local TA path of a repo.
+
+        Note that this method anonymizes the repository name.
+
+        :param ta_home: Path to home directory for all TA grading
+        :param ta_dirname: Name of directory for this assignment
+        :param repo: Name of the repository
+        :return: The anonymized local TA path of a repo
+        """
         return os.path.join(ta_home, ta_dirname, self.lookupTA(repo),
                             hashlib.sha1(repo.encode('utf-8')).hexdigest())
 
-    # yeah, we brute force these...
-    # fortunately, there aren't many to check
     def deanonymize_sha1_repo(self, anonrepo: str) -> str:
+        """Returns the deanonymized name of a repository
+
+        yeah, we brute force these...
+        fortunately, there aren't many to check
+
+        :param anonrepo: The anonymized SHA1 name of the repo
+        :return: the deanonymized name of a repository
+        """
         for repo in self.repo2group.keys():
             utf8_repo = repo.encode('utf-8')
             repohash = hashlib.sha1(utf8_repo).hexdigest()
